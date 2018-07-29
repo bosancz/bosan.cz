efc252823581d7d8ddeedf84073e4901bc17cc64
Martin Kopeček <github@smallhill.cz>
2018-07-29 14:29:45 +0200

    Upgrade group color pipe to group pipe

---
+++ src/app/pipes/group.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { DataService } from "../services/data.service";
+
+@Pipe({
+  name: 'group'
+})
+export class GroupPipe implements PipeTransform {
+  
+  groupIndex:any;
+  
+  constructor(dataService:DataService){
+    dataService.getGroups({fields:"_id,name,color"})
+      .then(groups => {
+        this.groupIndex = {};
+        groups.forEach(group => this.groupIndex[group._id] = group);
+      })
+      .catch(err => console.error(err));
+  }
+
+  transform(groupId:string,property:string):string{
+    if(!this.groupIndex[groupId]) return;
+    return this.groupIndex[groupId][property];
+  }
+
+}
