878bd6ac89a5329fe4fd03bd394e70e739f15518
Martin Kopeček <github@smallhill.cz>
2018-09-13 12:56:30 +0000

    GMAIL workin progress

--- src/mailings/index.js
+++ src/mailings/index.js
@@ -1,16 +1,48 @@
 var nodemailer = require("nodemailer");
+const {google} = require("googleapis"); 
 
 var config = require("../../config");
+
+const jwtClient = new google.auth.JWT(
+  config.google.mail,
+  null,
+  config.google.privateKey,
+  ['https://mail.google.com/'],
+  undefined
+);
 
 module.exports = async function (mailingId,params){
   
   var mailing = require(`./${mailingId}/${mailingId}`);
 
-  let transport = nodemailer.createTransport(config.mailing.transport,config.mailing.defaults);
-  
-  await mailing(transport,params);
-  
-  console.log(`Mailing ${mailingId} sent.`);
+  jwtClient.authorize((error, tokens) => {
     
-  transport.close();
+    console.log(error,tokens);
+    
+    if(error) return;
+    
+    var transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        type: 'OAuth2',
+        user: config.google.mail,
+        serviceClient: config.google.serviceClient,
+        privateKey: config.google.privateKey,
+        accessToken: tokens.access_token,
+        refreshToken: tokens.refresh_token,
+        expires: tokens.expiry_date
+      }
+    });
+
+    mailing(transport,params);
+
+    console.log(`Mailing ${mailingId} sent.`);
+    
+    transport.close();
+    
+  });
+  
+
 };
