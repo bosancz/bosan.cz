27a21070e98c8ed68c082fedf6b3931e4249c839
Martin Kopeček <github@smallhill.cz>
2018-09-17 11:58:32 +0000

    Display not found on unmatched route, fix deprecated tooltip placement

--- src/app/app.errorhandler.ts
+++ src/app/app.errorhandler.ts
@@ -24,9 +24,12 @@ export class AppErrorHandler implements ErrorHandler {
       } else {
         toastService.toast("Chyba serveru: " + err.message, "error");
       }
+    } else if(err.message.match("Cannot match any routes")) {
+      toastService.toast("Tato stránka neexistuje.", "error");
+      console.error({err});
     } else {
       toastService.toast("Nastala neočekávaná chyba :(", "error");
-      console.error(err);
+      console.error({err});
     }
   }
 
