7c802e51fcab741f51fc11a47e10ce10a81d6175
Martin Kopeček <github@smallhill.cz>
2018-09-17 08:04:15 +0000

    Mark unfinished parts

---
+++ src/app/admin/views/server-admin/server-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'server-admin',
+  templateUrl: './server-admin.component.html',
+  styleUrls: ['./server-admin.component.scss']
+})
+export class ServerAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
