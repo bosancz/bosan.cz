358ca82ad0af166ef6b728b9c774359088f17282
SmallhillCZ <github@smallhill.cz>
2018-11-08 17:58:34 +0000

    Fix bad variable reference

--- src/plugins/mongoose-actions.js
+++ src/plugins/mongoose-actions.js
@@ -3,8 +3,8 @@ const mongoParser = require("mongo-parse");
 
 function checkActionsExist(actions,names){
   for(let name of names){
-    if(!actions[findAction]){
-      const e = new Error("Invalid value for _actions: " + findAction);
+    if(!actions[name]){
+      const e = new Error("Invalid value for _actions: " + name);
       e.name = "MongooseActionsError";
       throw e;
     }   
@@ -26,14 +26,14 @@ function filterInDB(query,actions){
     checkActionsExist(actions,[findAction]);
     query.where(actions[findAction].query);
   }
-  
+
   delete query._conditions._actions;
 }
 
 module.exports = function(schema,options){
 
   const actions = options.actions;
-  
+
   // prepare parsed query for matching retrieved documens
   Object.entries(actions).forEach(action => {
     if(action[1].query) action[1].queryParsed = mongoParser.parse(action[1].query);
@@ -46,25 +46,24 @@ module.exports = function(schema,options){
 
   // add method action() for the Document objects
   schema.method("action",function(name,data){
-    actions[name].action(this.data);
+    actions[name].action(this,data);
   });
 
-  // add the hasAction query helper
-  schema.query.hasAction = function(name){
-    if(Array.isArray(name)) {
-      checkActionsExist(actions,name);
-      return this.where({ $or: name.map(name => actions[name].query) });
-    }
-    else{
-      checkActionsExist(actions,[name]);
-      return this.where(actions[name].query);
-    }
+  // add the hasAction(s) query helpers
+  schema.query.hasActions = function(names){
+    checkActionsExist(actions,names);
+    return this.where(names.length > 1 ? { $or: names.map(name => actions[name].query) } : actions[names[0]].query);
   }
-  
+
+  schema.query.hasAction = function(name){
+    return this.hasActions([name]);
+  }
+
+
   schema.query.hasAnyAction = function(name){
     return this.where({ $or: Object.entries(actions).map(action => action[1].query) });
   }
-  
+
   // hooks to find to enable in database filtering of events
   schema.pre("find",function(){ return filterInDB(this,actions); });
   schema.pre("findOne",function(){ return filterInDB(this,actions); });
