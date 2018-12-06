11e617b76d3ef0f74765aec3497d2ab2902ade62
Martin Kopeček <github@smallhill.cz>
2018-12-06 11:45:37 +0000

    Allow program admin to publish events

---
+++ src/acl/custom/program.js
@@ -0,0 +1,12 @@
+module.exports = {
+  can: {
+    
+    "events:list": true,
+    "events:read": true,
+    "events:edit": true,
+    "events:create": true,
+    "events:delete": true,
+    "events:publish": true,
+    
+  }
+};
