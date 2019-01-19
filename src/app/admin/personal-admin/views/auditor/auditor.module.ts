8bef1ac9f35c2930e462d9e24e11e93f982cf3c8
SmallhillCZ <github@smallhill.cz>
2019-01-19 18:10:24 +0100

    Add chart module to auditor module

--- src/app/admin/personal-admin/views/auditor/auditor.module.ts
+++ src/app/admin/personal-admin/views/auditor/auditor.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import "assets/lib/Chart.min.js";
+
+import { ChartsModule } from 'ng2-charts';
 
 import { AuditorRoutingModule } from './auditor-routing.module';
 import { EventReportsComponent } from './event-reports/event-reports.component';
 import { AuditorComponent } from './auditor.component';
+import { EventsDashboardComponent } from './events-dashboard/events-dashboard.component';
 
 @NgModule({
-  declarations: [EventReportsComponent, AuditorComponent],
+  declarations: [EventReportsComponent, AuditorComponent, EventsDashboardComponent],
   imports: [
     CommonModule,
-    AuditorRoutingModule
+    AuditorRoutingModule,
+    FormsModule,
+    ChartsModule
   ]
 })
 export class AuditorModule { }
