63b706fe1a588e2059e762cbef3e2a1e2274524e
SmallhillCZ <github@smallhill.cz>
2019-02-26 11:52:13 +0100

    Fix bad album download link, closes #160

--- src/app/frontend/views/gallery-view/gallery-view-album/gallery-view-album.component.ts
+++ src/app/frontend/views/gallery-view/gallery-view-album/gallery-view-album.component.ts
@@ -10,16 +10,16 @@ import { Album, Photo } from "app/shared/schema/album";
 
 import { GalleryPhoto } from "../../../components/photo-gallery/photo-gallery.component";
 
-class GalleryPhotoContainer implements GalleryPhoto{
-  
-  photo:Photo;
-  
+class GalleryPhotoContainer implements GalleryPhoto {
+
+  photo: Photo;
+
   // GalleryPhoto
-  url:string;
-  width:number;
-  height:number;
-  caption:string;
-  bg:string;
+  url: string;
+  width: number;
+  height: number;
+  caption: string;
+  bg: string;
 }
 @Component({
   selector: 'gallery-view-album',
@@ -28,59 +28,59 @@ class GalleryPhotoContainer implements GalleryPhoto{
 })
 export class GalleryViewAlbumComponent implements OnInit, OnDestroy {
 
-  album:Album;
-  
-  tags:string[] = [];
-  tag:string;
-  
-  galleryPhotos:GalleryPhoto[] = []; 
-  
-  paramsSubscription:Subscription;
-  
-  constructor(private api:ApiService, private toastService:ToastService, private router:Router, private route:ActivatedRoute) { }
+  album: Album;
+
+  tags: string[] = [];
+  tag: string;
+
+  galleryPhotos: GalleryPhoto[] = [];
+
+  paramsSubscription: Subscription;
+
+  constructor(private api: ApiService, private toastService: ToastService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.paramsSubscription = this.route.params.subscribe((params:Params) => {
-      
-      if(!this.album || params.album !== this.album._id) this.loadAlbum(params.album);
-      
-      if(params.tag !== this.tag){
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+
+      if (!this.album || params.album !== this.album._id) this.loadAlbum(params.album);
+
+      if (params.tag !== this.tag) {
         this.tag = params.tag;
         this.updateGalleryPhotos();
       }
-      
+
     });
   }
-  
-  ngOnDestroy(){
+
+  ngOnDestroy() {
     this.paramsSubscription.unsubscribe();
   }
-  
-  async loadAlbum(id:string){
-    this.album = await this.api.get<Album>(["galleryalbum",id]);
-    
-    if(!this.album) return this.toastService.toast("Album nenalezeno :(");
-                                                   
+
+  async loadAlbum(id: string) {
+    this.album = await this.api.get<Album>(["galleryalbum", id]);
+
+    if (!this.album) return this.toastService.toast("Album nenalezeno :(");
+
     this.updateTags();
     this.updateGalleryPhotos();
   }
-  
-  updateTags(){
-    
+
+  updateTags() {
+
     let tagIndex = {};
-    
+
     this.album.photos.forEach(photo => {
       photo.tags.forEach(tag => tagIndex[tag] = 1);
     });
-    
+
     this.tags = Object.keys(tagIndex);
   }
-  
-  updateGalleryPhotos(){
+
+  updateGalleryPhotos() {
     // album not yet loaded
-    if(!this.album) return;
-    
-    let galleryPhotos = this.album.photos.filter(photo => this.hasTag(photo,this.tag)).map(photo => ({
+    if (!this.album) return;
+
+    let galleryPhotos = this.album.photos.filter(photo => this.hasTag(photo, this.tag)).map(photo => ({
       photo: photo,
       url: photo.sizes.small.url,
       width: photo.sizes.small.width,
@@ -88,18 +88,22 @@ export class GalleryViewAlbumComponent implements OnInit, OnDestroy {
       caption: photo.caption,
       bg: photo.bg
     }));
-    
+
     this.galleryPhotos = galleryPhotos;
   }
-  
-  hasTag(photo:Photo,tag:string){
-    if(!tag) return true;
-    if(!photo.tags) return false;
+
+  hasTag(photo: Photo, tag: string) {
+    if (!tag) return true;
+    if (!photo.tags) return false;
     return photo.tags.indexOf(tag) !== -1;
   }
-  
-  openPhoto(container:GalleryPhotoContainer){
-    this.router.navigate(['./' + container.photo._id],{relativeTo:this.route});
+
+  openPhoto(container: GalleryPhotoContainer) {
+    this.router.navigate(['./' + container.photo._id], { relativeTo: this.route });
+  }
+
+  getAlbumDownloadLink(album: Album): string {
+    return this.api.link2href(album._links.download);
   }
 
 
