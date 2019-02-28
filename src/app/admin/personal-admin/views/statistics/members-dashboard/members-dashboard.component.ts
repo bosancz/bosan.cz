0544a9c3e81e2de699fa22e8c2e22606f1d0b026
SmallhillCZ <github@smallhill.cz>
2019-02-28 15:34:17 +0100

    Add members age and roles chart

--- src/app/admin/personal-admin/views/statistics/members-dashboard/members-dashboard.component.ts
+++ src/app/admin/personal-admin/views/statistics/members-dashboard/members-dashboard.component.ts
@@ -7,10 +7,16 @@ export interface MembersReport {
   count: number;
 
   roles: { [role: string]: number };
-  ages: { [age: string]: number };
+  ages: { [role: string]: { [age: string]: number } };
   cities: { [city: string]: number };
 
 }
+
+interface ChartData {
+  labels: string[];
+  datasets: { data: number[], label?: string }[];
+}
+
 
 @Component({
   selector: 'members-dashboard',
@@ -22,8 +28,20 @@ export class MembersDashboardComponent implements OnInit {
   report: MembersReport;
 
   roles: string[];
-  ages: string[];
   cities: string[];
+
+  agesData: ChartData;
+
+  agesOptions = {
+    scales: {
+      xAxes: [{
+        stacked: true,
+      }],
+      yAxes: [{
+        stacked: true
+      }]
+    }
+  }
 
   constructor(private api: ApiService, private config: ConfigService) { }
 
@@ -35,13 +53,41 @@ export class MembersDashboardComponent implements OnInit {
   async loadReport() {
     this.report = await this.api.get<MembersReport>("reports:members");
 
-    this.ages = Object.keys(this.report.ages);
     this.cities = Object.keys(this.report.cities);
+
+    this.updateAgesData();
+
   }
 
   async loadMemberRoles() {
     const config = await this.config.getConfig();
     this.roles = config.members.roles.map(role => role.id);
+  }
+
+  fillMissingKeys(data: { [key: string]: number }, min: number, max: number) {
+    for (let i = min; i <= max; i++) data[i] = data[i] || 0;
+  }
+
+  updateAgesData() {
+
+    if (!this.report || !this.report.ages) return this.agesData = undefined;
+
+    const min = Math.min(...Object.values(this.report.ages).map(roleAges => Math.min(...Object.keys(roleAges).map(age => Number(age)))));
+    const max = Math.max(...Object.values(this.report.ages).map(roleAges => Math.max(...Object.keys(roleAges).map(age => Number(age)))));
+
+    const agesData: ChartData = {
+      labels: [],
+      datasets: []
+    };
+
+    for (let i = min; i <= max; i++) agesData.labels.push(String(i));
+
+    for (let role of Object.entries(this.report.ages)) {
+      for (let i = min; i <= max; i++) role[1][i] = role[1][i] || 0;
+      agesData.datasets.push({ data: Object.values(role[1]), label: role[0] })
+    }
+
+    this.agesData = agesData;
   }
 
 }
