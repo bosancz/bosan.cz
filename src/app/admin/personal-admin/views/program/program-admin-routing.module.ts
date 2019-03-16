4be47c0fef1e359648a8e3f26d3aa995f4be0e4b
SmallhillCZ <github@smallhill.cz>
2019-03-16 21:54:21 +0100

    Open event detail in program not in my events

--- src/app/admin/personal-admin/views/program/program-admin-routing.module.ts
+++ src/app/admin/personal-admin/views/program/program-admin-routing.module.ts
@@ -7,23 +7,27 @@ import { ProgramDraftsComponent } from "./views/program-drafts/program-drafts.co
 import { EventApprovalComponent } from "./views/event-approval/event-approval.component";
 import { EventPlanningComponent } from "./views/event-planning/event-planning.component";
 import { EventProgramComponent } from "./views/event-program/event-program.component";
+import { MyEventComponent } from '../my-event/my-event.component';
 
 const routes: Routes = [
 
   {
     path: '', component: ProgramAdminComponent,
     children: [
-      {path: 'v-priprave', component: ProgramDraftsComponent},  
-      
-      {path: 'ke-schvaleni', component: EventApprovalComponent},  
-      
-      {path: 'v-programu', component: EventProgramComponent},  
 
-      {path: 'planovani', component: EventPlanningComponent},  
+      { path: 'detail/:akce', component: MyEventComponent },
 
-      {path: '', redirectTo: "ke-schvaleni", pathMatch: "full"}
+      { path: 'v-priprave', component: ProgramDraftsComponent },
+
+      { path: 'ke-schvaleni', component: EventApprovalComponent },
+
+      { path: 'v-programu', component: EventProgramComponent },
+
+      { path: 'planovani', component: EventPlanningComponent },
+
+      { path: '', redirectTo: "ke-schvaleni", pathMatch: "full" }
     ]
-  },  
+  },
 
 ];
 
