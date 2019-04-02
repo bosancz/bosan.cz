1126eec409ee02f21618c7994664e845e40fd010
Jarda Havelik <j.havelik@gmail.com>
2019-04-02 19:59:55 +0200

    Change hardcoded Troja GDocs links to variables from WebConfig

--- src/app/frontend/views/events-view/events-view.component.ts
+++ src/app/frontend/views/events-view/events-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TitleService } from "app/core/services/title.service";
+import { ConfigService } from "app/core/services/config.service";
 
 @Component({
   selector: 'events-view',
@@ -9,10 +10,20 @@ import { TitleService } from "app/core/services/title.service";
 })
 export class EventsViewComponent implements OnInit {
 
-  constructor(private titleService:TitleService) { }
+  constructor(private titleService:TitleService,private configService:ConfigService) { }
+
+  canalFormUrl:String;
+  canalAttendeesUrl:String;
 
   ngOnInit() {
     this.titleService.setTitle("Program");
+    this.loadConfig();
   }
 
+    private loadConfig() {
+        this.configService.getConfig().then(config => {
+            this.canalFormUrl = config.canal.formUrl;
+            this.canalAttendeesUrl = config.canal.attendeesUrl;
+        });
+    }
 }
