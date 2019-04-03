a92b02f3d9f36de7098d119e5f6630aa1fcb83c7
Jarda Havelik <j.havelik@gmail.com>
2019-04-03 20:43:59 +0200

    Fix loading attendees and form url by PR comments

--- src/app/frontend/views/events-view/events-view.component.ts
+++ src/app/frontend/views/events-view/events-view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { TitleService } from "app/core/services/title.service";
 import { ConfigService } from "app/core/services/config.service";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'events-view',
@@ -10,20 +12,12 @@ import { ConfigService } from "app/core/services/config.service";
 })
 export class EventsViewComponent implements OnInit {
 
-  constructor(private titleService:TitleService,private configService:ConfigService) { }
+  public canalFormUrl:Observable<string> = this.configService.config.pipe(map(config => config.canal.formUrl));
+  public canalAttendeesUrl:Observable<string> = this.configService.config.pipe(map(config => config.canal.attendeesUrl));
 
-  canalFormUrl:String;
-  canalAttendeesUrl:String;
+  constructor(private titleService:TitleService,private configService:ConfigService) { }
 
   ngOnInit() {
     this.titleService.setTitle("Program");
-    this.loadConfig();
   }
-
-    private loadConfig() {
-        this.configService.getConfig().then(config => {
-            this.canalFormUrl = config.canal.formUrl;
-            this.canalAttendeesUrl = config.canal.attendeesUrl;
-        });
-    }
 }
