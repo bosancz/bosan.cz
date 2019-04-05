03c68cf0231557cde35330bce73b7647c1481c81
SmallhillCZ <github@smallhill.cz>
2019-04-05 12:54:32 +0200

    Add local server environment config

---
+++ client/src/environments/environment.local-server.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+
+  apiRoot: "http://localhost:4300",
+
+  gapi: {
+    client_id: '249555539983-j8rvff7bovgnecsmjffe0a3dj55j33hh.apps.googleusercontent.com',
+    cookiepolicy: 'single_host_origin',
+    scope: 'profile email'
+  }
+};
