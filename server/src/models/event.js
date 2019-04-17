4dba997aedcbef5799faca7ab4c23029a819dfc9
SmallhillCZ <github@smallhill.cz>
2019-04-17 23:37:57 +0200

    Add rejected status to model

--- server/src/models/event.js
+++ server/src/models/event.js
@@ -8,7 +8,7 @@ const EventRecurring = require("./event-recurring"); // load because of referenc
 
 var eventSchema = mongoose.Schema({
 
-  "status": { type: String, enum: ['draft','pending','public','cancelled'], required: true, default: 'draft' },  
+  "status": { type: String, enum: ['draft','pending','public','cancelled','rejected'], required: true, default: 'draft' },  
   "statusNote": { type: String },
   
   "srcId": Number,
