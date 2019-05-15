751e23e767a1701591abc2099ead62f94ef54cbd
SmallhillCZ <github@smallhill.cz>
2019-05-16 00:36:43 +0200

    Wrap leaders nicely

--- client/src/app/shared/pipes/join-leaders.pipe.ts
+++ client/src/app/shared/pipes/join-leaders.pipe.ts
@@ -7,15 +7,21 @@ import { FormatPhonePipe } from "./format-phone.pipe";
 })
 export class JoinLeadersPipe implements PipeTransform {
 
-  transform(value:any[], phone:boolean = true):string{
-    
-    if(!value || !value.length) return "";
-    
+  transform(value: any[], showPhone: boolean = true, html: boolean = true): string {
+
+    if (!value || !value.length) return "";
+
     const formatPhonePipe = new FormatPhonePipe();
-    
-    const leadersStrings = value.map(leader => leader.nickname + (phone && leader.contacts && leader.contacts.mobile ? " (" + formatPhonePipe.transform(leader.contacts.mobile,"short",true) + ")" : ""));
-    
-    return leadersStrings.length > 1 ? leadersStrings.slice(0,leadersStrings.length - 1).join(", ") + " a " + leadersStrings[leadersStrings.length - 1] : leadersStrings[0];
+
+    const leadersStrings = value
+      .map(leader => {
+        let leaderString = leader.nickname
+        if(showPhone && leader.contacts && leader.contacts.mobile) leaderString += " (" + formatPhonePipe.transform(leader.contacts.mobile, "short", true) + ")";
+        return leaderString;
+      })
+      .map(leaderString => html ? leaderString.replace(/ /g, "&nbsp;") : leaderString);
+
+    return leadersStrings.length > 1 ? leadersStrings.slice(0, leadersStrings.length - 1).join(", ") + (html ? " a&nbsp;" : " a ") + leadersStrings[leadersStrings.length - 1] : leadersStrings[0];
   }
 
 }
