7d7e652da9ac0bf3af9a886588a020252380bf13
SmallhillCZ <github@smallhill.cz>
2020-04-02 15:21:02 +0200

    Fix secondary menu view on PC

---
+++ client-admin/src/app/shared/directives/secondary-menu.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, OnInit, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
+import { MenuService } from 'app/services/menu.service';
+
+@Directive({
+  selector: '[secondaryMenu]'
+})
+export class SecondaryMenuDirective implements OnInit, OnDestroy  {
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+    private menuService: MenuService,
+  ) {
+
+  }
+
+  ngOnInit() {
+    
+    // render for large display
+    this.viewContainer.createEmbeddedView(this.templateRef);
+    
+    // display in menu for mobile
+    this.menuService.secondaryMenu = this.templateRef;
+  }
+
+  ngOnDestroy() {
+    this.menuService.secondaryMenu = null;
+  }
+
+
+}
