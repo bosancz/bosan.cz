538f39c2b2fc5395e39758de7f1ceb33f0e22997
SmallhillCZ <github@smallhill.cz>
2020-04-06 20:19:41 +0200

    Fix client-admin routing

--- server/src/router.js
+++ server/src/router.js
@@ -2,7 +2,7 @@ const config = require("./config");
 
 const { Routes, RoutesLinks } = require("@smallhillcz/routesjs");
 
-const routes = new Routes({ url: config.server.baseDir });
+const routes = new Routes({ url: "" });
 
 module.exports = routes.router;
 
