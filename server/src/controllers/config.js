8187aaf7203c930e83dee88422e19cb94bc40bfb
SmallhillCZ <github@smallhill.cz>
2020-04-06 20:18:53 +0200

    Fix missing config file on clean start

--- server/src/controllers/config.js
+++ server/src/controllers/config.js
@@ -8,7 +8,14 @@ var path = require("path");
 
 var configFile = path.resolve(config.storage.config, "web-config.json");
 
-routes.get("config", "/", { permission: "config:read" }).handle((req, res, next) => res.sendFile(configFile));
+routes.get("config", "/", { permission: "config:read" }).handle(async (req, res, next) => {
+  if (await fs.pathExists(configFile)) {
+    res.sendFile(configFile)
+  }
+  else {
+    res.json({});
+  }
+});
 
 routes.put("config", "/", { permission: "config:edit" }).handle(async (req, res, next) => {
   await fs.writeFile(configFile, JSON.stringify(req.body));
