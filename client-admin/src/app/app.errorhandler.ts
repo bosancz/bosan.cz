453a2b23c2fb5663c23f08c03232abc9922b64b3
SmallhillCZ <github@smallhill.cz>
2020-04-07 18:34:19 +0200

    Dont send error reports from developing

--- client-admin/src/app/app.errorhandler.ts
+++ client-admin/src/app/app.errorhandler.ts
@@ -16,7 +16,7 @@ export class AppErrorHandler implements ErrorHandler {
 
   handleError(err: any) {
 
-    const onlineService = this.injector.get(OnlineService);    
+    const onlineService = this.injector.get(OnlineService);
     const api = this.injector.get(ApiService);
     const router = this.injector.get(Router);
 
@@ -36,14 +36,14 @@ export class AppErrorHandler implements ErrorHandler {
         environment: environment.production ? "production" : "development"
       }
     };
-    
+
     if (err instanceof HttpErrorResponse) {
-      
-      errorData.description = JSON.stringify(err.error,undefined,"  ");
+
+      errorData.description = JSON.stringify(err.error, undefined, "  ");
 
       if (!onlineService.online.value) return; // dont report errors due to conenction loss
 
-      if (err.status === 401) {        
+      if (err.status === 401) {
         router.navigate(["/login"]);
       }
       else if (err.status === 403) {
@@ -53,18 +53,18 @@ export class AppErrorHandler implements ErrorHandler {
       console.error(err);
 
     }
-    else if(err.name === "GoogleError"){
-      if(err.message === "idpiframe_initialization_failed"){
-        console.error("Failed to initialize Google Services");    
+    else if (err.name === "GoogleError") {
+      if (err.message === "idpiframe_initialization_failed") {
+        console.error("Failed to initialize Google Services");
         reportError = false;
       }
       else console.error(err);
     }
-    else {      
+    else {
       console.error(err);
     }
 
-    if(reportError){
+    if (reportError && environment.production) {
       api.post("errors", errorData).catch(err => console.error(err));
     }
   }
