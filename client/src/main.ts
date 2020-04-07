bccb3f4cbdd4a98bebc003aa74a7d24aec11d557
SmallhillCZ <github@smallhill.cz>
2020-04-07 11:47:38 +0200

    Set default locale to cs

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, LOCALE_ID } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [{ provide: LOCALE_ID, useValue: 'cs' }]
+})
