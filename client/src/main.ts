45619fb72fffc437986a36d58352686a3a5c2f6c
SmallhillCZ <github@smallhill.cz>
2020-05-12 12:20:48 +0200

    Try to remove obsolete service workers from client on startup

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// remove previous service workers for frontend only
+if (window.navigator && window.navigator.serviceWorker) {
+
+  window.navigator.serviceWorker.getRegistrations()
+    .then(registrations => registrations.filter(registration => registration.scope === "https://bosan.cz/" || registration.scope === "/" || !registration.scope))
+    .then(registrations => {
+      if (registrations.length) {
+        console.log("Found obsolete service workers, removing and reloading web...")
+        registrations.forEach(registration => registration.unregister());
+        window.location.reload();
+      }
+    });
+
+}
+
+
 if (environment.production) {
   enableProdMode();
 }
