fa0ab7870630493cca13cc2e36327a765076c00f
Kryštof Řezáč <krystofrezac@gmail.com>
2020-06-27 19:49:21 +0200

    add swagger

--- new-server/src/main.ts
+++ new-server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import AppModule from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
