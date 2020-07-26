5c1611aa80724ab9af14fc079a60beb8ec12cc74
Martin Kopeček <github@smallhill.cz>
2020-07-26 20:41:03 +0200

    Fix/edit leaders (#306)
    * Fix prevent creating of tests
    
    * Add leaders editing page
    
    * Change placeholder wording

--- client-admin/src/app/shared/components/members-select/members-select.component.ts
+++ client-admin/src/app/shared/components/members-select/members-select.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, Input, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { COMMA, ENTER, SPACE } from '@angular/cdk/keycodes';
 
 import { ConfigService } from "app/services/config.service";
 import { ApiService } from "app/services/api.service";
 
 import { Member } from "app/shared/schema/member";
 import { WebConfigGroup } from "app/shared/schema/web-config";
+import { Subject, combineLatest, BehaviorSubject } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+
+type MemberWithSearchString = Member & { searchString: string };
 
 @Component({
   selector: 'members-select',
@@ -19,88 +26,88 @@ import { WebConfigGroup } from "app/shared/schema/web-config";
     }
   ]
 })
-export class MembersSelectComponent implements OnInit, ControlValueAccessor {
+export class MembersSelectComponent implements ControlValueAccessor {
 
-  @Input() role: string;
+  value$ = new Subject<string[]>();
+  members$ = new Subject<MemberWithSearchString[]>();
 
-  members: Member[] = [];
-  selectedMembers: Member[] = [];
+  selectedMembers$ = new BehaviorSubject<Member[]>([]);
 
-  group: string = "";
-  search: RegExp;
-  searchIndex: string[];
+  searchIndex: string[] = [];
 
-  groups: WebConfigGroup[] = [];
-  roles: string[] = [];
+  search$ = new Subject<string>();
+  filteredMembers = combineLatest(this.search$.pipe(filter(search => typeof search === "string")), this.members$)
+    .pipe(map(([search, members]) => {
+      const searchString = this.replaceSpecialChars(search);
+      return members.filter(member => member.searchString.search(searchString) !== -1);
+    }));
 
+  /* autocomplete */
+  separatorKeysCodes = [ENTER, COMMA, SPACE];
+
+  /* ngModel */
   disabled: boolean = false;
 
   onChange: any = () => { };
   onTouched: any = () => { };
 
-  writeValue(members: Member[]): void { this.selectedMembers = members || []; }
+  writeValue(membersIds: string[]): void {
+    this.value$.next(membersIds || []);
+  }
   registerOnChange(fn: any): void { this.onChange = fn; }
   registerOnTouched(fn: any): void { this.onTouched = fn; }
   setDisabledState(isDisabled: boolean): void { this.disabled = isDisabled; }
-
-  constructor(private api: ApiService, private configService: ConfigService) {
+  publishValue() {
+    this.onChange(this.selectedMembers$.value.map(member => member._id));
   }
 
-  ngOnInit() {
-    this.loadMembers();
-    this.loadConfig();
+  constructor(
+    private api: ApiService,
+    private configService: ConfigService
+  ) {
+
+    combineLatest(this.members$, this.value$)
+      .pipe(map(([members, value]) => {
+        return value.map(id => members.find(item => item._id === id)).filter(item => !!item)
+      }))
+      .subscribe(this.selectedMembers$);
+
   }
 
-  ngOnChanges() {
-
+  @Input("members")
+  set setMembers(members: Member[]) {
+    this.members$.next(members.map(member => ({ ...member, searchString: this.getSearchString(member) })));
   }
 
-  async loadMembers() {
-    const options = {
-      role: this.role,
-      sort: "group -role nickname"
-    }
+  getSearchString(member: Member): string {
+    let names = member.nickname + (member.name ? (" " + member.name.first + " " + member.name.last) : "");
 
-    this.members = await this.api.get<Member[]>("members", options);
-    // TODO: diacritics insensitive search
-    this.searchIndex = this.members.map(member => {
-      let names = member.nickname + (member.name ? (" " + member.name.first + " " + member.name.last) : "");
-
-      names = names.toLowerCase();
-      names = this.specialCharsReplace(names);
-      return names;
-    });
+    names = names.toLowerCase();
+    names = this.replaceSpecialChars(names);
+    return names;
   }
 
-  loadConfig() {
-    this.configService.getConfig().then(config => {
-      this.groups = config.members.groups;
-    });
-  }
-
-  setSearch(search: string) {
-    search = this.specialCharsReplace(search);
-    if (search) this.search = new RegExp(search.toLowerCase());
-    else this.search = null;
-  }
-
-  isHidden(member: Member, i: number): boolean {
-    return (this.search && !this.search.test(this.searchIndex[i])) || (this.group && member.group !== this.group);
-  }
-
-  selectedMember(member: Member): boolean {
-    return this.selectedMembers.some(item => item._id === member._id);
-  }
-
-  toggleMember(member: Member, select: boolean): void {
-    if (select && !this.selectedMember(member)) this.selectedMembers.push(member);
-    if (!select) this.selectedMembers = this.selectedMembers.filter(item => item._id !== member._id);
-    this.onChange(this.selectedMembers);
-  }
-
-  specialCharsReplace(string: string) {
+  replaceSpecialChars(string: string) {
     const replaceChars = ["áčďéěíľňóřšťúůýž", "acdeeilnorstuuyz"];
     return string.replace(/[áčďéěíľňóřšťúůýž]/g, char => replaceChars[1].charAt(replaceChars[0].indexOf(char)));
+  }
+
+  add(event: MatAutocompleteSelectedEvent) {
+    const member = event.option.value;
+    const index = this.selectedMembers$.value.indexOf(member);
+
+    if (index === -1) {
+      this.selectedMembers$.next([...this.selectedMembers$.value, member]);
+    }
+
+    this.publishValue();
+    this.search$.next("");
+  }
+
+  remove(member: Member) {
+    this.selectedMembers$.next(this.selectedMembers$.value.filter(item => item !== member));
+
+    this.publishValue();
   }
 
 }
--- client-admin/src/app/views/events/events-routing.module.ts
+++ client-admin/src/app/views/events/events-routing.module.ts
@@ -5,11 +5,12 @@ import { EventsListComponent } from './views/events-list/events-list.component';
 import { EventsViewComponent } from './views/events-view/events-view.component';
 import { MyEventsComponent } from './views/my-events/my-events.component';
 
+import { EventEditAccountingComponent } from './views/event-edit/event-edit-accounting/event-edit-accounting.component';
+import { EventEditAttendeesComponent } from './views/event-edit/event-edit-attendees/event-edit-attendees.component';
 import { EventEditComponent } from './views/event-edit/event-edit.component';
 import { EventEditInfoComponent } from './views/event-edit/event-edit-info/event-edit-info.component';
+import { EventEditLeadersComponent } from './views/event-edit/event-edit-leaders/event-edit-leaders.component';
 import { EventEditRegistrationComponent } from './views/event-edit/event-edit-registration/event-edit-registration.component';
-import { EventEditAttendeesComponent } from './views/event-edit/event-edit-attendees/event-edit-attendees.component';
-import { EventEditAccountingComponent } from './views/event-edit/event-edit-accounting/event-edit-accounting.component';
 import { EventEditReportComponent } from './views/event-edit/event-edit-report/event-edit-report.component';
 
 const routes: Routes = [
@@ -20,6 +21,7 @@ const routes: Routes = [
     children: [
       { path: "info", component: EventEditInfoComponent },
       { path: "prihlaska", component: EventEditRegistrationComponent },
+      { path: "vedouci", component: EventEditLeadersComponent },
       { path: "ucastnici", component: EventEditAttendeesComponent },
       { path: "uctovani", component: EventEditAccountingComponent },
       { path: "report", component: EventEditReportComponent },
