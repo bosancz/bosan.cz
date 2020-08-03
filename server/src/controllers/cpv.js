02ce570a68ad61e75507139c19f2812921b84b43
Martin Kopeček <github@smallhill.cz>
2020-08-03 12:34:10 +0200

    Change source of CPV events to Padler, closes #253 (#308)

--- server/src/controllers/cpv.js
+++ server/src/controllers/cpv.js
@@ -1,69 +1,36 @@
 const { Routes } = require("@smallhillcz/routesjs");
-const request = require("request-promise-native");
+const axios = require("axios");
 const cheerio = require('cheerio');
 const { DateTime } = require("luxon");
 
 const routes = module.exports = new Routes();
 
-routes.get("cpv:kanoe","/kanoe.cz", {  }).handle(async (req,res) => {
-  var cpvPage = await request({
-    uri: "https://www.kanoe.cz/terminy/turistika",
-    gzip: true
-  });
+routes.get("cpv", "/", {}).handle(async (req, res) => {
+  var padlerResponse = await axios("https://www.padler.cz/kalendar-akci/", { responseType: "text" });
 
-  const $ = cheerio.load(cpvPage)
-
-  const table = $("table").first();
+  const $ = cheerio.load(padlerResponse.data, { xmlMode: true })
 
   const events = [];
 
-  table.find("tbody tr").each((i,elem) => {
-    const name = $(elem).find("td").eq(0).text().trim();
-    const date = $(elem).find("td").eq(1).text().trim();
-    if(name && date){
-      const match = date.match(/((\d+)\. ?((\d+)\.)?\-)?(\d+)\. ?(\d+)\. ?(\d+)$/);
-      if(!match) return;
+  $("#groups tr").each((i, elem) => {
+    const fields = $(elem).find("td");
 
-      const dateFrom = DateTime.fromObject({ year: Number(match[7]), month: Number(match[4] || match[6]), day: Number(match[2] || match[5]) });
-      const dateTill = DateTime.fromObject({ year: Number(match[7]), month: Number(match[6]), day: Number(match[5]) });
-
-      events.push({
-        name: name,
-        dateFrom: dateFrom.toISODate(),
-        dateTill: dateTill.toISODate(),
-        link: "https://www.kanoe.cz/terminy/turistika"
-      });
-    }
-  });
-
-  res.json(events)
-});
-
-routes.get("cpv:raft","/raft.cz", {  }).handle(async (req,res) => {
-  var cpvPage = await request({
-    uri: "https://www.raft.cz/export_akce.ashx"
-  });
-
-  const $ = cheerio.load(cpvPage, { xmlMode: true })
-
-  const events = [];
-
-  $("akce").each((i,elem) => {
-    const name = $(elem).find("Nazev").text();
-    var dateFrom = $(elem).find("Datum_od").text();
-    var dateTill = $(elem).find("Datum_do").text();
+    const name = fields.eq(1).find("a").text().trim();
+    const link = fields.eq(1).find("a").attr("href");
+    const dateFrom = fields.eq(0).find("a").text().trim();
+    const dateParts = dateFrom.match(/(\d{2})\.&nbsp;(\d{2}).&nbsp;(\d{4})/, " ");
 
 
-    if(name && dateFrom){
-      dateFrom = DateTime.fromFormat(dateFrom,"dd.LL.yyyy");
-      dateTill = DateTime.fromFormat(dateTill,"dd.LL.yyyy");
+    if (name && dateParts) {
+
+      const date = DateTime.fromObject({ year: dateParts[3], month: dateParts[2], day: dateParts[1] });
 
       events.push({
-        name: name,
-        dateFrom: dateFrom.toISODate(),
-        dateTill: dateTill.toISODate(),
-        description: $(elem).find("Popis").text(),
-        link: $(elem).find("Odkaz").text()
+        source: "padler",
+        name,
+        dateFrom: date.toISODate(),
+        dateTill: date.toISODate(),
+        link: "https://padler.cz"+ link
       });
     }
   });
