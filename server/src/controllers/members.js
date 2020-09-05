bb198f2ce2985c1d25d07cf0a264e05dba8261e2
Martin Kopeček <github@smallhill.cz>
2020-09-05 14:34:55 +0200

    Fix missing member links

--- server/src/controllers/members.js
+++ server/src/controllers/members.js
@@ -5,39 +5,41 @@ const routes = module.exports = new Routes();
 
 var Member = require("../models/member");
 
-routes.get("members","/",{permission:"members:list"}).handle(async (req,res,next) => {
-  
-  var query = Member.find({});  
-  query.filterByPermission("members:list",req);
-  
-  if(req.query.role) query.where({"role": Array.isArray(req.query.role) ? { $in: req.query.role } : req.query.role});
-  if(req.query.group) query.where({"group": req.query.group});
-  if(req.query.sort) query.sort(req.query.sort);
-  if(req.query.select) query.select(req.query.select);
-  
+routes.get("members", "/", { permission: "members:list" }).handle(async (req, res, next) => {
+
+  var query = Member.find({});
+  query.filterByPermission("members:list", req);
+
+  if (req.query.role) query.where({ "role": Array.isArray(req.query.role) ? { $in: req.query.role } : req.query.role });
+  if (req.query.group) query.where({ "group": req.query.group });
+  if (req.query.sort) query.sort(req.query.sort);
+  if (req.query.select) query.select(req.query.select);
+
   const members = await query.toObject();
-  
-  req.routes.links(members,"member");
-  
+
+  req.routes.links(members, "member");
+
   res.json(members);
 });
 
-routes.post("members","/",{permission:"members:list"}).handle(async (req,res) => {
+routes.post("members", "/", { permission: "members:list" }).handle(async (req, res) => {
   var member = await Member.create(req.body);
   res.location(`/members/${member._id}`);
   res.status(201).json(member);
 });
 
-routes.get("member","/:id",{permission:"members:read"}).handle(async (req,res,next) => {
-  res.json(await Member.findOne({_id:req.params.id}));
+routes.get("member", "/:id", { permission: "members:read" }).handle(async (req, res, next) => {
+  const member = (await Member.findOne({ _id: req.params.id })).toObject();
+  req.routes.links(member, "member");
+  res.json(member);
 });
 
-routes.patch("member","/:id",{permission:"members:edit"}).handle(async (req,res,next) => {
-  await Member.findOneAndUpdate({_id:req.params.id},req.body)
+routes.patch("member", "/:id", { permission: "members:edit" }).handle(async (req, res, next) => {
+  await Member.findOneAndUpdate({ _id: req.params.id }, req.body)
   res.sendStatus(204);
 });
 
-routes.delete("member","/:id",{permission:"members:delete"}).handle(async (req,res,next) => {
-  await Member.remove({_id:req.params.id})
+routes.delete("member", "/:id", { permission: "members:delete" }).handle(async (req, res, next) => {
+  await Member.remove({ _id: req.params.id })
   res.sendStatus(204);
 });
