19c695e0bb85e9b71b617e20da71d488a1f92356
Martin Kopeček <github@smallhill.cz>
2020-09-05 14:20:47 +0200

    Fix bad sameSite assignment

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -22,7 +22,7 @@ module.exports = {
   cookieName: "access_token",
   cookieMaxAge: expiration.as("milliseconds"),
   cookieSecure: !!JSON.parse(process.env.AUTH_SECURE || 'true'),
-  cookieSameSite: (process.env.AUTH_SAMESITE === "true" ? true : process.env.AUTH_SAMESITE) || true,
+  cookieSameSite: (process.env.AUTH_SAMESITE === "true" ? true : (process.env.AUTH_SAMESITE === "false" ? false : process.env.AUTH_SAMESITE)) || true,
 
   bcrypt: {
     rounds: 12
