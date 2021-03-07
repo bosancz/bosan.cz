ae7e6804ba20d8a2766401107ccbc917e11d56df
Martin Kopeček <github@smallhill.cz>
2021-03-07 14:44:47 +0100

    add default sharing image

--- server/src/config/general.js
+++ server/src/config/general.js
@@ -1,12 +1,19 @@
 const server = require("./server");
 const { DateTime } = require("luxon");
 
+const url = process.env.BASE_URL || `https://${server.host}${server.port ? ':' + server.port : ''}`;
+
 module.exports = {
 
   title: process.env.SITE_TITLE || "Dětská vodácká skupina ŠÁN",
   description: process.env.SITE_DESCRIPTION || "Skupina Šán je seskupení sedmi dětských vodáckých oddílů. Domovskou loděnici máme v Podolí mezi Žlutými lázněmi a botelem Racek naproti plaveckému bazénu.",
   mail: process.env.SITE_MAIL || "info@bosan.cz",
 
-  url: process.env.BASE_URL || `https://${server.host}${server.port ? ':' + server.port : ''}`
+  url: url,
 
+  image: {
+    url: url + "/assets/img/headers/kayaks.jpg",
+    width: 2560,
+    height: 1438
+  }
 }
--- server/src/controllers/share.js
+++ server/src/controllers/share.js
@@ -9,13 +9,13 @@ var createPage = require("./share/create-page");
 var Album = require("../models/album");
 var Photo = require("../models/photo");
 
-routes.get(null,"/fotogalerie/:album",{permission:"gallery:read"}).handle(async (req,res) => {
-  
-  const album = await Album.findOne({_id:req.params.album}).populate("titlePhotos");
-  
+routes.get(null, "/fotogalerie/:album", { permission: "gallery:read" }).handle(async (req, res) => {
+
+  const album = await Album.findOne({ _id: req.params.album }).populate("titlePhotos");
+
   const dateFrom = DateTime.fromJSDate(album.dateFrom).toFormat("d. M. y");
   const dateTill = DateTime.fromJSDate(album.dateFrom).toFormat("d. M. y");
-  
+
   const options = {
     url: `${config.url}/fotogalerie/${req.params.album}`,
     title: album.name + " (" + (dateFrom === dateTill ? dateFrom : dateFrom + " - " + dateTill) + ")",
@@ -27,20 +27,20 @@ routes.get(null,"/fotogalerie/:album",{permission:"gallery:read"}).handle(async 
       height: album.titlePhotos && album.titlePhotos[0] ? album.titlePhotos[0].sizes.big.height : ""
     }
   }
-  
+
   res.type("html").send(createPage(options));
 });
 
-routes.get(null, "/fotogalerie/:album/:photo",{permission:"gallery:read"}).handle(async (req,res) => {
-  
-  var album = await Album.findOne({_id:req.params.album});
-  var photo = await Photo.findOne({_id:req.params.photo});
-  
-  if(!photo || !album) return res.sendStatus(404);
-  
+routes.get(null, "/fotogalerie/:album/:photo", { permission: "gallery:read" }).handle(async (req, res) => {
+
+  var album = await Album.findOne({ _id: req.params.album });
+  var photo = await Photo.findOne({ _id: req.params.photo });
+
+  if (!photo || !album) return res.sendStatus(404);
+
   const dateFrom = DateTime.fromJSDate(album.dateFrom).toFormat("d. M. y");
   const dateTill = DateTime.fromJSDate(album.dateFrom).toFormat("d. M. y");
-  
+
   var options = {
     url: `${config.url}/fotogalerie/${req.params.album}/${req.params.photo}`,
     title: album.name + " (" + (dateFrom === dateTill ? dateFrom : dateFrom + " - " + dateTill) + ")",
@@ -52,30 +52,32 @@ routes.get(null, "/fotogalerie/:album/:photo",{permission:"gallery:read"}).handl
       height: photo.sizes.big.height
     }
   }
-  
+
   res.type("html").send(createPage(options));
 });
 
-routes.get(null, "/program", {permission:"webinfo:read"}).handle((req,res) => {
-  
+routes.get(null, "/program", { permission: "webinfo:read" }).handle((req, res) => {
+
   var options = {
-    url:  `${config.url}/program`,
+    url: `${config.url}/program`,
     title: "Program akcí :: " + config.general.title,
     description: "Podívejte se, jaké akce pro vás máme připraveny na příštích pár týdnů.",
-    type: "website"
+    type: "website",
+    image: config.general.image
   };
-  
+
   res.type("html").send(createPage(options));
 });
 
-routes.get(null, "**", {permission:"webinfo:read"}).handle((req,res) => {
-  
+routes.get(null, "**", { permission: "webinfo:read" }).handle((req, res) => {
+
   var options = {
     url: config.general.url,
     title: config.general.title,
-    description: config.description,
-    type: "website"
+    description: config.general.description,
+    type: "website",
+    image: config.general.image
   };
-  
+
   res.type("html").send(createPage(options));
 });
