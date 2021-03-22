c106bf5f77f782f00418f4d51480ee4d711de19d
Kryštof Řezáč <krystofrezac@gmail.com>
2021-03-22 11:55:32 +0100

    Add export button

--- client-admin/src/app/views/events/views/events-list/events-list.component.ts
+++ client-admin/src/app/views/events/views/events-list/events-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from "@angular/forms";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
-import { ApiService } from "app/services/api.service";
+import {ApiService} from "app/services/api.service";
 
-import { Event } from "app/shared/schema/event";
-import { DateTime } from 'luxon';
-import { WebConfigEventStatus } from 'app/shared/schema/web-config';
-import { ConfigService } from 'app/services/config.service';
-import { Subject, Observable, combineLatest, BehaviorSubject } from 'rxjs';
-import { debounceTime, map } from 'rxjs/operators';
+import {Event} from "app/shared/schema/event";
+import {DateTime} from 'luxon';
+import {WebConfigEventStatus} from 'app/shared/schema/web-config';
+import {ConfigService} from 'app/services/config.service';
+import {Subject, Observable, combineLatest, BehaviorSubject} from 'rxjs';
+import {debounceTime, map} from 'rxjs/operators';
 
 type EventWithSearchString = Event & { searchString?: string };
 
@@ -29,23 +29,23 @@ export class EventsListComponent implements OnInit {
 
   canCreate: boolean;
 
-  @ViewChild('filterForm', { static: true }) filterForm: NgForm;
+  @ViewChild('filterForm', {static: true}) filterForm: NgForm;
 
   showFilter: boolean = false;
 
   search$ = new BehaviorSubject<string>("");
 
   constructor(
-    private api: ApiService,
-    private configService: ConfigService
+      private api: ApiService,
+      private configService: ConfigService
   ) {
 
     api.resources
-      .then(resources => resources.events.allowed.POST)
-      .then(canCreate => this.canCreate = canCreate);
+        .then(resources => resources.events.allowed.POST)
+        .then(canCreate => this.canCreate = canCreate);
 
     this.filteredEvents$ = combineLatest(this.events$, this.search$.pipe(debounceTime(250)))
-      .pipe(map(([events, search]) => this.filterEvents(events, search)));
+        .pipe(map(([events, search]) => this.filterEvents(events, search)));
 
   }
 
@@ -70,9 +70,9 @@ export class EventsListComponent implements OnInit {
   async loadYears() {
     this.years = await this.api.get<number[]>("events:years");
     this.years.sort((a, b) => b - a);
-    
+
     const thisYear = DateTime.local().year;
-    if(this.years.indexOf(thisYear) !== -1) this.currentYear = thisYear;
+    if (this.years.indexOf(thisYear) !== -1) this.currentYear = thisYear;
     else this.currentYear = this.years[0];
   }
 
@@ -85,8 +85,8 @@ export class EventsListComponent implements OnInit {
     };
 
     options.filter = {
-      dateTill: { $gte: DateTime.local().set({ year: filter.year, month: 1, day: 1 }).toISODate() },
-      dateFrom: { $lte: DateTime.local().set({ year: filter.year, month: 12, day: 31 }).toISODate() }
+      dateTill: {$gte: DateTime.local().set({year: filter.year, month: 1, day: 1}).toISODate()},
+      dateFrom: {$lte: DateTime.local().set({year: filter.year, month: 12, day: 31}).toISODate()}
     }
 
     if (filter.status) options.filter.status = filter.status;
@@ -106,16 +106,27 @@ export class EventsListComponent implements OnInit {
   }
 
   filterEvents(events: EventWithSearchString[], search: string) {
-    
-    if(!search) return events;
-    
+
+    if (!search) return events;
+
     const search_re = new RegExp("(^| )" + search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), "i")
-    
+
     return events.filter(event => search_re.test(event.searchString))
   }
 
   getLeadersString(event: Event) {
     return event.leaders.map(item => item.nickname).join(", ");
+  }
+
+  async export() {
+    const options: any = {
+      sort: "dateFrom",
+      select: "description"
+    };
+
+    const events: EventWithSearchString[] = await this.api.get<Event[]>("events", options);
+
+    console.log("eventsWithDescription", events)
   }
 
 }
