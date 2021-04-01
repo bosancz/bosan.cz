ecd2df0a802f2f9a578813652e06286cdceb9e10
Martin Kopeček <github@smallhill.cz>
2021-04-01 13:07:24 +0200

    create trimester select and doc export UI

--- client-admin/src/app/views/program/views/program-print/program-print.component.ts
+++ client-admin/src/app/views/program/views/program-print/program-print.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'app/services/api.service';
+import { ProgramExportService } from 'app/services/program-export.service';
+import { Event } from 'app/shared/schema/event';
 
 @Component({
   selector: 'bo-program-print',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgramPrintComponent implements OnInit {
 
-  constructor() { }
+  dateRange?: [string, string];
+
+  constructor(
+    private api: ApiService,
+    private programExport: ProgramExportService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  async exportProgram(dateRange) {
+    const requestOptions = {
+      filter: {
+        dateFrom: { $lte: this.dateRange[1] },
+        dateTill: { $gte: this.dateRange[0] },
+      },
+      select: "_id name description dateFrom dateTill leaders"
+    };
+
+    const events = await this.api.get<Event[]>("events", requestOptions);
+
+    this.programExport.export(events);
   }
 
 }
