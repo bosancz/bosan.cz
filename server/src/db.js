f7301bda04d627877cd6b8b7947ee26f0644201c
Martin Kopeček <github@smallhill.cz>
2021-04-01 13:49:20 +0200

    fix logging DB URI at startup

--- server/src/db.js
+++ server/src/db.js
@@ -1,6 +1,6 @@
 var config = require("./config");
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 /* PLUGINS */
 mongoose.plugin(require("mongoose-autopopulate"));
@@ -18,25 +18,23 @@ mongoose.Promise = global.Promise;
 var retries = 0;
 
 function connectDB(attempt) {
+  if (!attempt) console.log("[DB] DB URI: " + config.database.uri);
 
   attempt = (attempt || 0) + 1;
 
-  if (!attempt) console.log("[DB] DB URI: " + config.database.uri)
-
   console.log(`[DB] Connecting to DB... attempt ${attempt}/${config.database.retryCount}`);
 
-  mongoose.connect(config.database.uri, { useNewUrlParser: true })
+  mongoose
+    .connect(config.database.uri, { useNewUrlParser: true })
     .then(() => console.log("[DB] Connected to " + config.database.uri))
-    .catch(err => {
+    .catch((err) => {
       if (attempt <= config.database.retryCount) {
         console.error(`[DB] Error when connectiong to DB. Will retry in ${config.database.retryInterval / 1000}s.`);
         setTimeout(() => connectDB(attempt), config.database.retryInterval);
-      }
-      else {
+      } else {
         throw new Error("[DB] Error when connectiong to DB: " + err.message);
       }
     });
-
 }
 
 /* MODELS */
@@ -45,5 +43,5 @@ require("./models");
 
 module.exports = {
   connectDB,
-  mongoose
-}
+  mongoose,
+};
