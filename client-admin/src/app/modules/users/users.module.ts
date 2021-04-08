be31502e244252e85087269bdb4e7a1cf1088558
Martin Kopeček <github@smallhill.cz>
2021-04-08 12:06:46 +0200

    remove unused material module

--- client-admin/src/app/modules/users/users.module.ts
+++ client-admin/src/app/modules/users/users.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { UsersRoutingModule } from './users-routing.module';
-import { UsersViewComponent } from './users-view/users-view.component';
-import { UsersListComponent } from './users-list/users-list.component';
-
+import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
 import { UsersCreateComponent } from './users-create/users-create.component';
-import { MaterialModule } from 'app/shared/material.module';
 import { UsersEditComponent } from './users-edit/users-edit.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersViewComponent } from './users-view/users-view.component';
+
+
 
 @NgModule({
   declarations: [
@@ -22,7 +21,6 @@ import { UsersEditComponent } from './users-edit/users-edit.component';
     UsersRoutingModule,
 
     SharedModule,
-    MaterialModule,
   ]
 })
 export class UsersModule { }
