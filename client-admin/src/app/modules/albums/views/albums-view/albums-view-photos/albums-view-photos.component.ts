12a6b5cb9d018c30f96a4c9793932f23a36cb1eb
Martin Kopeček <github@smallhill.cz>
2021-04-27 17:06:05 +0200

    enable photo deleting and ordering, improve layout

--- client-admin/src/app/modules/albums/views/albums-view/albums-view-photos/albums-view-photos.component.ts
+++ client-admin/src/app/modules/albums/views/albums-view/albums-view-photos/albums-view-photos.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { ModalController, Platform, ViewWillLeave } from '@ionic/angular';
 import { ItemReorderEventDetail } from '@ionic/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { ToastService } from 'app/core/services/toast.service';
 import { PhotoViewComponent } from 'app/modules/albums/components/photo-view/photo-view.component';
 import { PhotosUploadComponent } from 'app/modules/albums/components/photos-upload/photos-upload.component';
 import { Album, Photo } from 'app/schema/album';
@@ -19,7 +19,9 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
 
   album?: Album<Photo, string>;
 
-  photos?: Photo[];
+  photos?: Photo[] = [];
+
+  loadingPhotos?: any[] = Array(5).fill(true);
 
   actions: Action[] = [
 
@@ -28,20 +30,26 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
   enableOrdering = false;
   enableDeleting = false;
 
+  showCheckboxes = false;
+  selectedPhotos: Photo[] = [];
+
   modal?: HTMLIonModalElement;
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
     private albumsService: AlbumsService,
     public platform: Platform,
-    public modalController: ModalController
+    public modalController: ModalController,
+    private toastService: ToastService
   ) { }
 
   ngOnInit(): void {
     this.albumsService.album$
       .pipe(untilDestroyed(this))
-      .subscribe(album => this.loadPhotos(album));
+      .subscribe(album => {
+        this.album = album;
+        this.actions = this.getActions(this.album);
+        this.loadPhotos(album);
+      });
   }
 
   ionViewWillLeave() {
@@ -49,10 +57,8 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
   }
 
   async loadPhotos(album: Album<Photo, string>) {
-    this.album = album;
-    this.photos = undefined;
     this.photos = await this.albumsService.getPhotos(album);
-    this.actions = this.getActions(this.album);
+    this.loadingPhotos = undefined;
   }
 
   async onPhotoClick(photo: Photo, event: MouseEvent) {
@@ -71,32 +77,54 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
     this.modal.present();
   }
 
-  private startOrdering() {
+
+  onPhotoCheck(photo: Photo, isChecked: boolean) {
+    const i = this.selectedPhotos.indexOf(photo);
+
+    // if checked, but not in list, add photo 
+    if (isChecked && i === -1) this.selectedPhotos.push(photo);
+
+    // if not checked, but in list, remove photo
+    else if (!isChecked && i !== -1) this.selectedPhotos.splice(i, 1);
+
+    console.log(this.selectedPhotos);
+  }
+
+  onReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    this.photos?.splice(ev.detail.to, 0, this.photos?.splice(ev.detail.from, 1)[0]);
+    ev.detail.complete();
+  }
+
+  startOrdering() {
     this.enableOrdering = true;
     this.actions = [{
-      "text": "Hotovo",
+      "text": "Uložit",
+      color: "primary",
+      pinned: true,
+      handler: () => this.saveOrdering().then(() => this.endOrdering())
+    },
+    {
+      text: "Zrušit",
+      hidden: this.platform.is('ios'),
       handler: () => this.endOrdering()
     }];
   }
 
-  async onReorder(ev: CustomEvent<ItemReorderEventDetail>) {
-    ev.detail.complete();
-  }
-
-  private endOrdering() {
+  endOrdering() {
     this.actions = this.getActions(this.album!);
-    console.log(this.getActions(this.album!));
     this.enableOrdering = false;
   }
 
-  private startDeleting() {
+  startDeleting() {
+    this.startSelecting();
     this.enableDeleting = true;
     this.actions = [
       {
         text: "Smazat",
         role: "destructive",
         color: "danger",
-        handler: () => this.deletePhotos()
+        pinned: true,
+        handler: () => this.deletePhotos().then(() => this.endDeleting())
       },
       {
         text: "Zrušit",
@@ -106,13 +134,44 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
     ];
   }
 
-  private deletePhotos() {
-    this.endDeleting();
-  }
-
   endDeleting() {
     this.enableDeleting = false;
+    this.stopSelecting();
     this.actions = this.getActions(this.album!);
+  }
+
+
+  getMpix(width: number, height: number) {
+    return Math.round(width * height / 1000000);
+  }
+
+  private startSelecting() {
+    this.showCheckboxes = true;
+    this.selectedPhotos = [];
+  }
+
+  private stopSelecting() {
+    this.showCheckboxes = false;
+    this.selectedPhotos = [];
+  }
+
+
+  private async deletePhotos() {
+
+    const toast = await this.toastService.toast("Mažu fotky...");
+
+    for (let photo of this.selectedPhotos) {
+      await this.albumsService.deletePhoto(photo._id);
+    }
+
+    await this.loadPhotos(this.album!); // wouldnt be able to delete photos if no album was present
+
+    toast.dismiss();
+    this.toastService.toast("Fotky smazány");
+  }
+
+  private async deletePhoto(photo: Photo) {
+    await this.albumsService.deletePhoto(photo._id);
   }
 
   private async uploadPhotos() {
@@ -131,10 +190,18 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
     this.modal.onDidDismiss().then(saved => saved && this.albumsService.loadAlbum(this.album!._id));
   }
 
+  private async saveOrdering() {
+    if (!this.album || !this.photos) return;
 
+    const data: Pick<Album<string>, "photos"> = {
+      photos: this.photos.map(photo => photo._id)
+    };
 
-  private async deletePhoto() {
+    await this.albumsService.updateAlbum(this.album._id, data);
 
+    await this.loadPhotos(this.album);
+
+    this.toastService.toast("Uloženo.");
   }
 
   private getActions(album: Album<Photo, string>): Action[] {
@@ -145,12 +212,12 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
         handler: () => this.startOrdering()
       },
       {
-        text: "Nahrát fotky",
+        text: "Nahrát",
         icon: "cloud-upload-outline",
         handler: () => this.uploadPhotos()
       },
       {
-        text: "Smazat fotky",
+        text: "Smazat",
         color: "danger",
         role: "destructive",
         icon: "trash-outline",
@@ -160,7 +227,4 @@ export class AlbumsViewPhotosComponent implements OnInit, ViewWillLeave {
     ];
   }
 
-  getMpix(width: number, height: number) {
-    return Math.round(width * height / 1000000);
-  }
-}
+};
