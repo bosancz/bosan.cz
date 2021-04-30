7b9386bd2871d7450bf008a4b27d0e76307a8261
Martin Kopeček <github@smallhill.cz>
2021-04-30 20:35:50 +0200

    improve schema colors usage

--- client-admin/src/app/config/event-statuses.ts
+++ client-admin/src/app/config/event-statuses.ts
@@ -11,7 +11,7 @@ export const EventStatuses = asEventStatuses({
 
   "draft": {
     "name": "Připravovaná",
-    "color": "light"
+    "color": "medium"
   },
   "pending": {
     "name": "Čeká na schválení",
