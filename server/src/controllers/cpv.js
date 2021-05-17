38fe7ad9afadf4808457fc22ef0b99b2374c4f79
Martin Kopeček <github@smallhill.cz>
2021-05-17 13:52:55 +0200

    add dateTill to new padler export

--- server/src/controllers/cpv.js
+++ server/src/controllers/cpv.js
@@ -24,6 +24,7 @@ routes.get("cpv", "/", {}).handle(async (req, res) => {
           source: "padler",
           name,
           dateFrom,
+          dateTill: dateFrom,
           link,
         });
       });
