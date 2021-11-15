696f9de6acc8183b357a04958b34c70be10f3cb5
Martin Kopeček <martin.kopecek@smallhill.cz>
2021-11-15 23:45:38 +0100

    fix event images

--- client/src/app/shared/pipes/event.pipe.ts
+++ client/src/app/shared/pipes/event.pipe.ts
@@ -4,6 +4,7 @@ import { ConfigService } from "app/services/config.service";
 
 import { Event } from "app/shared/schema/event";
 import { WebConfigEventType, WebConfigEventSubType } from "app/shared/schema/webconfig";
+import { EventTypes } from 'config/event-types';
 
 type EventPipeProperty = "image" | "color" | "class";
 
@@ -13,41 +14,16 @@ type EventPipeProperty = "image" | "color" | "class";
 })
 export class EventPipe implements PipeTransform {
 
-  eventTypes: {
-    [name: string]: WebConfigEventType
-  } = {};
-
-  eventSubTypes: {
-    [name: string]: WebConfigEventSubType
-  } = {};
-
-  constructor(
-    private configService: ConfigService,
-    private cdRef: ChangeDetectorRef
-  ) {
-    this.loadEvents();
-  }
-
-  loadEvents() {
-    this.configService.getConfig().then(config => {
-      this.eventTypes = config.events.types.reduce((acc, cur) => ({ ...acc, [cur.name]: cur }), {});
-      this.eventSubTypes = config.events.subtypes.reduce((acc, cur) => ({ ...acc, [cur.name]: cur }), {});
-      this.cdRef.markForCheck();
-    });
-
-  }
+  constructor() { }
 
   transform(event: Event, property: EventPipeProperty): string {
     if (!event) return;
 
     switch (property) {
 
-      case "class":
-        return this.eventTypes[event.type] && this.eventTypes[event.type][property] || "";
-
       case "color":
       case "image":
-        return this.eventSubTypes[event.subtype] && this.eventSubTypes[event.subtype][property] || "";
+        return EventTypes[event.subtype] && EventTypes[event.subtype][property] || "";
 
       default:
         return "?";
