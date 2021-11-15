4f68afe608f2d83b5a0660ccf31dd50c7ff00285
Martin Kopeček <martin.kopecek@smallhill.cz>
2021-11-15 23:45:47 +0100

    fix event group colors

--- client/src/app/shared/pipes/group.pipe.ts
+++ client/src/app/shared/pipes/group.pipe.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform, Injectable } from '@angular/core';
 
 import { ConfigService } from "app/services/config.service";
+import { MemberGroups } from 'config/member-groups';
 
 export type GroupPipeProperty = "name" | "color";
 
@@ -10,33 +11,15 @@ export type GroupPipeProperty = "name" | "color";
 })
 export class GroupPipe implements PipeTransform {
 
-  groupIndex: any = {};
-
-  defaultValues: { [key: string]: any } = {
-    "color": "#000"
-  };
-
-  constructor(private configService: ConfigService) {
-    this.loadGroups();
-  }
-
-  loadGroups() {
-    this.configService.config.subscribe(config => {
-
-      // create group index with properties
-      this.groupIndex = {};
-      config.members.groups.forEach(group => this.groupIndex[group.id] = group);
-    })
-  }
+  constructor() { }
 
   transform(groupId: string, property: GroupPipeProperty): string {
 
-    // if group properties not loaded yet or not present for group, return default values
     switch (property) {
       case "name":
-        return this.groupIndex[groupId] && this.groupIndex[groupId][property] || groupId;
-      default:
-        return (this.groupIndex[groupId] ? this.groupIndex[groupId][property] : this.defaultValues[property]) || "";
+        return MemberGroups[groupId]?.[property] || groupId;
+      case "color":
+        return MemberGroups[groupId]?.[property] || "#000";
     }
   }
 
