254a6009c7065fea69e8367c4a75a4bf0478e863
Martin Kopeček <github@smallhill.cz>
2022-12-14 14:59:27 +0000

    deps: angular 15

--- interni/src/app/app-routing.module.ts
+++ interni/src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', paramsInheritanceStrategy: "always" })],
+  imports: [RouterModule.forRoot(routes, { paramsInheritanceStrategy: "always" })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
