9310467306da96d11e0d8d0c1017584a8af98ce2
Martin Kopeček <martin.kopecek@smallhill.cz>
2023-04-18 13:50:38 +0000

    fix: month name in program, closes #323

--- web/src/app/shared/components/events-timeline/events-timeline.component.ts
+++ web/src/app/shared/components/events-timeline/events-timeline.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
-import { ConfigService } from "app/services/config.service";
 import { ApiService } from "app/services/api.service";
 
 import { Event } from "app/shared/schema/event";
 
 @Component({
-  selector: 'events-timeline',
+  selector: "events-timeline",
   templateUrl: "events-timeline.component.html",
-  styleUrls: ["events-timeline.component.scss"]
+  styleUrls: ["events-timeline.component.scss"],
 })
 export class EventsTimelineComponent implements OnInit {
-
   @Input() limit: number;
 
   @Input() groupsFilter: boolean;
@@ -22,27 +20,43 @@ export class EventsTimelineComponent implements OnInit {
 
   loading: boolean = false;
 
-  months = ["Leden", "Únor", "Březen", "Duben", "Květen", "Červen", "Červenec", "Srpen", "Září", "Říjen", "Listopad", "Prosinec"];
+  private months = [
+    "Leden",
+    "Únor",
+    "Březen",
+    "Duben",
+    "Květen",
+    "Červen",
+    "Červenec",
+    "Srpen",
+    "Září",
+    "Říjen",
+    "Listopad",
+    "Prosinec",
+  ];
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService) {}
 
   ngOnInit() {
     this.loadEvents();
   }
 
   async loadEvents() {
-
     this.loading = true;
 
     this.events = await this.api.get<Event[]>("program", { limit: this.limit || undefined });
 
-    this.events.forEach(event => event.groups.sort((a, b) => a.localeCompare(b, undefined, { numeric: true })));
+    this.events.forEach((event) => event.groups.sort((a, b) => a.localeCompare(b, undefined, { numeric: true })));
 
     this.loading = false;
-
   }
 
   getEventRegistrationUrl(event: Event): string {
     return this.api.link2href(event._links.registration);
+  }
+
+  getMonth(date: Date | string) {
+    const i = new Date(date).getMonth();
+    return this.months[i];
   }
 }
