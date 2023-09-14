570d6ef89b88d1f9c354b4284af054439efce78c
Grejdy <grejdy@bosan.cz>
2023-09-14 23:37:41 +0200

    Přidání návodu k odebírání kalendáře akcí

--- web/src/app/views/events-view/events-view.component.ts
+++ web/src/app/views/events-view/events-view.component.ts
@@ -4,6 +4,9 @@ import { TitleService } from "app/services/title.service";
 import { ConfigService } from "app/services/config.service";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+import { CalendarSyncManualComponent } from "app/components/calendar-sync-manual/calendar-sync-manual.component";
 
 @Component({
   selector: 'events-view',
@@ -15,9 +18,15 @@ export class EventsViewComponent implements OnInit {
   public canalFormUrl: Observable<string> = this.configService.config.pipe(map(config => config.general.canalFormUrl));
   public canalAttendeesUrl: Observable<string> = this.configService.config.pipe(map(config => config.general.canalAttendeesUrl));
 
-  constructor(private titleService: TitleService, private configService: ConfigService) { }
+  IcalManualRef:BsModalRef;
+
+  constructor(private titleService: TitleService, private configService: ConfigService, private IcalManualService: BsModalService) { }
 
   ngOnInit() {
     this.titleService.setPageTitle("Program");
+  }
+
+  OpenManualForIcal(){
+    this.IcalManualRef = this.IcalManualService.show(CalendarSyncManualComponent, Object.assign({}, { class: 'gray modal-lg' }));
   }
 }
